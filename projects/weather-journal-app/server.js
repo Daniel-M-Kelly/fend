94a8e482941161639ee62cfb00cc61ca3b06dc20
Daniel Kelly <email@danielmkelly.com>
2021-01-16 16:22:48 -0800

    Project Start

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+
+const app = express();
+
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+
+listening = () => console.log(`Server running on port:${port}`);
+
+const server = app.listen(port, listening)
+
+// Variable for Open Weather Map API Key
+const OWMApiKey = '46d47cb411bbb0290b9ff89a6610aaed'
