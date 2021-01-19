d731df2d6f7675def760d1c71c522807ce9f98fb
Daniel Kelly <email@danielmkelly.com>
2021-01-18 17:10:55 -0800

    Start project

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -28,7 +28,26 @@ const port = 8080;
 
 listening = () => console.log(`Server running on port:${port}`);
 
-const server = app.listen(port, listening)
+const server = app.listen(port, listening);
+
+// One API base url
+const baseURL = 'api.openweathermap.org/data/2.5/weather?'
 
 // Variable for Open Weather Map API Key
-const OWMApiKey = '46d47cb411bbb0290b9ff89a6610aaed'
+const OWMApiKey = '46d47cb411bbb0290b9ff89a6610aaed';
+
+/* Get Route 1 
+    Server Side GET route to return projectData
+*/
+app.get('/projData', (req, res) => {
+    res.send(projectData);
+});
+
+
+/* Post Route
+    Add user data 
+*/
+
+app.post('/submit', (req, res) => {
+    projectData.push(req.body)
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,27 @@
 /* Global Variables */
 
+const { response } = require("express");
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//
+const postData = async (url = '', data = {}) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content=type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
