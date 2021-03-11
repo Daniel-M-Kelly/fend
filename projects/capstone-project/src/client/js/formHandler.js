eb07e67ac9d98b97727ca11124a502cb8667bcf3
Daniel Kelly <email@danielmkelly.com>
2021-03-11 14:13:15 -0800

    feat: geonames api working

--- projects/capstone-project/src/client/js/formHandler.js
+++ projects/capstone-project/src/client/js/formHandler.js
@@ -2,11 +2,11 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // Get the URL from the form field
-    let formText = document.getElementById('url').value
+    let formText = document.getElementById('locationName').value
 
     // Build the post request function
-    const getSummary = async (data = {}) => {
-        const res = await fetch('http://localhost:8080/summary', {
+    const getLocation = async (data = {}) => {
+        const res = await fetch('http://localhost:8080/locationSearch', {
                 method: 'POST',
                 credentials: 'same-origin',
                 mode: 'cors',
@@ -16,27 +16,31 @@ function handleSubmit(event) {
                 body: JSON.stringify(data)
             });
             try {
-                const summary = await res.json();
-                return summary;
+                const locationInfo = await res.json();
+                return locationInfo;
             } catch (error) {
                 console.log('error', error);
             }
         };
     // Check if valid URL was Entered
-    if (Client.validURL(formText)) {
+    //if (Client.validURL(formText)) {
 
         // If valid URL, run getSummary function
-        getSummary({url: formText})
+        getLocation({location: formText})
         // When API response is received, update the results section of the page with the summary
         .then(
             function (res) {
-                document.getElementById('results').innerText = `Article Summary: ${res.summary}`;
+                document.getElementById('results').innerText = `
+                    Location Information: ${res["geonames"][0].name}
+                    Country: ${res["geonames"][0].countryName}
+                    Latitude: ${res["geonames"][0].lat}
+                    Longitude: ${res["geonames"][0].lng}`;
             }
         )
-    } else {
+    //} else {
         // Notify user of invalid URL
-        alert('Invalid URL');
-    }
+        //alert('Invalid URL');
+    //}
 };
 
 //Export function to include in index.js
--- projects/capstone-project/src/server/index.js
+++ projects/capstone-project/src/server/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors());
 
-app.use(bodyParser.json())
+app.use(express.json());
 
 app.use(express.static('dist'))
 
@@ -21,7 +21,7 @@ const pixabay_API_Key = process.env.pixabay_API_Key
 const geonames_username = process.env.geonames_username
 
 // API Call
-const baseURL = 'https://api.meaningcloud.com/summarization-1.0?key='
+const geonamesURL = 'http://api.geonames.org/searchJSON?q='
 
 
 
@@ -35,16 +35,17 @@ app.listen(8080, function () {
 })
 
 // Post route to get summary information from API
-app.post('/summary', async function (req, res) {
-    articleURL = req.body.url;
-    //console.log(`URL of article to summarize: ${articleURL}`)
+app.post('/locationSearch', async function (req, res) {
+    searchLocation = req.body.location;
+    console.log(`Location to Search for: ${searchLocation}`)
 
-    const api_res = await fetch(`${baseURL}${apiKey}&url=${articleURL}&sentences=5`);
-    
+    const api_res = await fetch(`${geonamesURL}${searchLocation}&maxRows=1&username=${geonames_username}`);
+    console.log(api_res)
+
     try {
-        const summaryData = await api_res.json();
-        res.send(summaryData)
-        //console.log(`Article Summary Sent! ${summaryData}`)
+        const apiData = await api_res.json();
+        res.send(apiData)
+        console.log(`Geonames api Data Sent! ${apiData}`)
     } catch (error) {
         console.log('Error: ', error);
     }
