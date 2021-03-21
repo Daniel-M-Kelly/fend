796c783cb75d88d681ab7cb7b9bbd3ebb97e1fb1
Daniel Kelly <email@danielmkelly.com>
2021-03-21 11:46:13 -0700

    Code Refactoring

--- projects/capstone-project/src/server/index.js
+++ projects/capstone-project/src/server/index.js
@@ -36,36 +36,25 @@ app.listen(8080, function () {
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-/* Get Route 1 
-    Server Side GET route to return data from array
-*/
-app.get('/all', sendData);
 
-function sendData (req, res) {
-    //console.log(`initial data ${projectData.location}`)
+//function sendData (req, res) {
+const sendData = (req, res) => {
     //Re-calculate DateDifference
     dateCalc (projectData.departDate);
-    /*let now = new Date();
-    let departDate = new Date(projectData.departDate);
-    let dateDiff = Math.floor((departDate - now) / (1000 * 60 * 60 * 24) + 1 );
-    projectData.dateDiff = dateDiff */
     res.send(projectData);
 };
 
-/* Post Route
-    Add journal entry to data array
-*/
-
-app.post('/addTrip', addTrip);
+// Get Route - Server Side GET route to return data from array
+app.get('/all', sendData);
 
 //Function to lookup data from APIs and save to projectData array
-async function addTrip (req, res) {
+const addTrip = async (req, res) => {
 
     //Calculate how many days from now until departure
-    dateCalc(req.body.departDate)
+    dateCalc(req.body.departDate);
 
     //Run locationSearch Function to get location Name, Latitude, and Logitude for use in subsequent APIs
-    const locData = await locationSearch(req.body.location)
+    const locData = await locationSearch(req.body.location);
 
     //Lookup weather information for the location
     await weatherLookup(locData["geonames"][0].lat, locData["geonames"][0].lng, projectData.dateDiff );
@@ -78,41 +67,46 @@ async function addTrip (req, res) {
 
 };
 
+// Post Route - Add trip to data array
+app.post('/addTrip', addTrip);
+
 //Function to get location information from geonames
-async function locationSearch (searchLoc) {
-    console.log(`Location to Search for: ${searchLoc}`)
+//async function locationSearch (searchLoc) {
+const locationSearch = async searchLoc => {
+    console.log(`Location to Search for: ${searchLoc}`);
     const api_res = await fetch(`${geonamesURL}${searchLoc}&maxRows=1&username=${geonames_username}`);
     try {
         const apiData = await api_res.json();
         //Add Location name, country, latitude, and longitude to project data array
         //Latitude and Longitued used to find weather forecast
-        projectData.location = apiData["geonames"][0].name
-        projectData.country = apiData["geonames"][0].countryName
-        projectData.latitude = apiData["geonames"][0].lat
-        projectData.longitude = apiData["geonames"][0].lng
-        return apiData
+        projectData.location = apiData["geonames"][0].name;
+        projectData.country = apiData["geonames"][0].countryName;
+        projectData.latitude = apiData["geonames"][0].lat;
+        projectData.longitude = apiData["geonames"][0].lng;
+        return apiData;
     } catch (error) {
         console.log('Error: ', error);
     }
 };
 
-
-async function pictureLookup (searchLoc) {
-    encodedLoc = encodeURI(searchLoc)
-    //console.log(encodedLoc)
-    //console.log(`${pixabayURL}${pixabay_API_Key}&q=${encodedLoc}&image_type=photo&safesearch=true`)
-    let api_res = await fetch(`${pixabayURL}${pixabay_API_Key}&q=${encodedLoc}&image_type=photo&safesearch=true`);
+//Function to lookup an image of the searched for location
+const pictureLookup = async searchLoc => {
+    //URI encode search location string 
+    encodedLoc = encodeURI(searchLoc);
+    
+    //Pixabay API image search
+    const api_res = await fetch(`${pixabayURL}${pixabay_API_Key}&q=${encodedLoc}&image_type=photo&safesearch=true`);
         try {
             const apiData = await api_res.json();
-            //console.log(`${apiData['hits'][0].webformatURL}`)
-            projectData.imgURL = apiData['hits'][0].webformatURL
+            //Take the first hit image from the query string
+            projectData.imgURL = apiData['hits'][0].webformatURL;
         } catch (error) {
             console.log('Error: ', error);
         }
 };
 
 //Look up weather from weatherbits.io API using latitude and longitude
-async function weatherLookup (lat,lng,dateDiff) {
+const weatherLookup = async (lat, lng, dateDiff) => {
 
     //If the depart date is within 16 days, get the weather forecast for that day
     if (dateDiff < 16) { 
@@ -120,21 +114,21 @@ async function weatherLookup (lat,lng,dateDiff) {
         let api_res = await fetch(`${weatherForecastURL}${lat}&lon=${lng}&key=${weatherBit_API_Key}`);
         try {
             const apiData = await api_res.json();
-            projectData.temp = apiData["data"][dateDiff].temp 
-            projectData.weather = apiData["data"][dateDiff]['weather'].description
-            projectData.weatherIcon = apiData["data"][dateDiff]['weather'].icon
+            projectData.temp = apiData["data"][dateDiff].temp;
+            projectData.weather = apiData["data"][dateDiff]['weather'].description;
+            projectData.weatherIcon = apiData["data"][dateDiff]['weather'].icon;
         } catch (error) {
             console.log('Error: ', error);
         }
 
     } else {
         //If the departure date is more than 16 days away, get the current weather.
-        let api_res = await fetch(`${weatherCurrentURL}${lat}&lon=${lng}&key=${weatherBit_API_Key}`);
+        const api_res = await fetch(`${weatherCurrentURL}${lat}&lon=${lng}&key=${weatherBit_API_Key}`);
         try {
             const apiData = await api_res.json();
-            projectData.temp = apiData["data"][0].temp
-            projectData.weather = apiData["data"][0]['weather'].description
-            projectData.weatherIcon = apiData["data"][0]['weather'].icon
+            projectData.temp = apiData["data"][0].temp;
+            projectData.weather = apiData["data"][0]['weather'].description;
+            projectData.weatherIcon = apiData["data"][0]['weather'].icon;
         } catch (error) {
             console.log('Error: ', error);
         }
